7cf83f2e07234670a96f0b3445acbbdca76fd341
rcarnevale <rcarnevale@hotmail.com>
2021-06-22 20:33:59 -0300

    removendo componentes obsoletos

--- dashboard/src/app/components/dashboard/dashboard.module.ts
+++ dashboard/src/app/components/dashboard/dashboard.module.ts
@@ -5,14 +5,10 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 import { DashboardListComponent } from "./dashboard-list/dashboard-list.component";
 import { HeaderComponent } from "./header/header.component";
-import { MenuComponent } from "./menu/menu.component";
-import { UserMenuComponent } from "./user-menu/user-menu.component";
 
 @NgModule({
     declarations: [
         HeaderComponent,
-        MenuComponent,
-        UserMenuComponent,
         DashboardListComponent
     ],
     imports: [
@@ -24,8 +20,6 @@ import { UserMenuComponent } from "./user-menu/user-menu.component";
     ],
     exports:[
         HeaderComponent,
-        MenuComponent,
-        UserMenuComponent,
         DashboardListComponent
     ]
 })
