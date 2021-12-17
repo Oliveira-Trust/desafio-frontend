5cadda53bf94b0e7ba191cf6881a715c0b012591
Gustavo Garcia <gustavo.ggdrn@gmail.com>
2021-12-17 16:12:22 -0300

    feat: Create store

--- src/services/index.js
+++ src/services/index.js
@@ -3,5 +3,5 @@ import { configAxios } from '@/utils/api-instance';
 export const getUsersList = async (queryString = '') => {
     const api = configAxios();
     const { data, headers: { xTotalCount } } = await api.get('users/' + queryString);
-    return { results: data, count: (xTotalCount / 10) }
+    return { results: data, count: (xTotalCount / 10), total: Number(xTotalCount) }
 }
