f4120f686455f51eb75610a5423ca6cfd6c541d4
Gustavo Garcia <gustavo.ggdrn@gmail.com>
2021-12-17 16:11:53 -0300

    feat: install font Awesome

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router';
+// icons
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 // components global
 import ComponentRegistration from '@/utils/component-registration';
 // css global
 import '@/scss/core.scss';
+// Registro de Icones
+library.add(fas);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 
 ComponentRegistration();
