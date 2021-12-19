e5bac8a1b57d84eac7cdaa5aa3741ce4e54dcb32
Gustavo Garcia <gustavo.ggdrn@gmail.com>
2021-12-19 18:32:51 -0300

    feat: Added Footer

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="page-flexbox-wrapper">
         <header-component />
         <main class="container mx-auto mb-10 sm:px-0 px-5">
             <ot-notification
@@ -9,16 +9,19 @@
             />
             <router-view />
         </main>
+        <footer-component />
     </div>
 </template>
 
 <script>
 import { mapState } from "vuex";
 import headerComponent from "./header.vue"
+import footerComponent from "./footer.vue"
 export default {
     name: 'Default',
     components: {
-        'header-component': headerComponent
+        'header-component': headerComponent,
+        'footer-component': footerComponent
     },
     computed: {
         ...mapState('notification', ['notification'])
@@ -29,4 +32,13 @@ export default {
 body {
     background: #f5f6f8;
 }
+</style>
+<style lang="sass">
+.page-flexbox-wrapper
+    display: flex
+    min-height: 100vh
+    flex-direction: column
+
+main
+    flex: 1 1 auto
 </style>
---
+++ src/layouts/footer.vue
@@ -0,0 +1,20 @@
+<template>
+    <footer>
+        <div class="flex justify-center items-center mt-10 mb-10">
+            <div class="title">Fintools | Desafio Front-End</div>
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'Footer'
+}
+</script>
+
+<style lang="scss" scoped>
+.title {
+    font-size: 14px;
+    color: #707070;
+}
+</style>
