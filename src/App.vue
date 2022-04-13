1f9ec980aaffb145361e8b0aeeb1754edce02ebf
MatheusT45 <matheus.tavares45@gmail.com>
2022-04-13 19:45:16 -0300

    feat: remove default vue elements

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <UsersTable/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import UsersTable from './components/UsersTable.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    UsersTable
+  },
 }
 </script>
 
---
+++ src/components/UsersTable.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="users-table">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UsersTable',
+}
+</script>
+
+<style scoped>
+p {
+  color: black;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+</style>
