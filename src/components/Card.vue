6cdb5118983cdb2681f33cc694324eff05511a4b
MatheusT45 <matheus.tavares45@gmail.com>
2022-04-13 22:12:56 -0300

    feat: add card component

---
+++ src/components/Card.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="card">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+}
+</script>
+
+<style scoped>
+.card {
+  background-color: #FFFFFF;
+  border-radius: 10px;
+  box-shadow: 2px 2px #AAAAAA;
+  padding: 20px;
+}
+</style>
