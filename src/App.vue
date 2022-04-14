7576b3b20754a16da2ac12565e850227c1567a54
MatheusT45 <matheus.tavares45@gmail.com>
2022-04-13 22:16:43 -0300

    feat: add navbar and card components to app

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,25 @@
 <template>
   <div id="app">
-    <UsersTable/>
+    <NavBar />
+    <div class="users-table-card">
+      <Card>
+        <UsersTable/>
+      </Card>
+    </div>
   </div>
 </template>
 
 <script>
 import UsersTable from './components/UsersTable.vue'
+import Card from './components/Card.vue'
+import NavBar from './components/NavBar.vue'
 
 export default {
   name: 'App',
   components: {
-    UsersTable
+    UsersTable,
+    Card,
+    NavBar,
   },
   async created () {
     await this.$store.dispatch('pullUsers');
@@ -18,13 +27,27 @@ export default {
 }
 </script>
 
+<style scoped>
+.users-table-card {
+  padding: 80px;
+}
+</style>
+
 <style>
+* {
+  padding: 0;
+  margin: 0;
+}
+
+html {
+  background-color: #F4F4F4;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  width: 100vw;
 }
 </style>
