0cc323f258a5efb12ee4968be05a013ca310f28a
MatheusT45 <matheus.tavares45@gmail.com>
2022-04-14 20:31:11 -0300

    feat: add new component IconButton

---
+++ src/components/IconButton.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="icon-button" @click="onClick">
+    <v-icon class="icon" :name="name"/>
+  </div>
+</template>
+
+<script>
+import 'vue-awesome/icons/pencil-alt'
+import 'vue-awesome/icons/trash-alt'
+import 'vue-awesome/icons/regular/user-circle'
+
+export default {
+  name: 'IconButton',
+  props: {
+    name: {
+      type: String,
+      default: 'pencil-alt'
+    }
+  },
+  methods: {
+    onClick() {
+      this.$emit('onClick');
+    }
+  }
+}
+</script>
+
+<style scoped>
+.icon-button {
+  height: 24px;
+  width: 24px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 3px 2px;
+  border-radius: 50%;
+  cursor: pointer;
+}
+
+.icon-button:hover {
+  background-color: #DFDFDF;
+}
+
+.icon {
+  fill: #656565;
+}
+</style>
