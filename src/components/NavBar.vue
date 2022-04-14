2dfe37be48a58f2a0f25bd17312fd9d3a714ff2e
MatheusT45 <matheus.tavares45@gmail.com>
2022-04-14 20:38:06 -0300

    refactor: update navbar with user profile, name and logout button

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,38 +1,52 @@
 <template>
   <div class="nav-bar">
-    <div class="nav-bar__content">
-      <div class="nav-bar__content__logomark">
-        <img src="../assets/logo.png" class="logo"/>
+    <div class="nav-bar__contents">
+      <div class="nav-bar__content">
+        <img src="../assets/logo.png" alt="Oliveira Trust Logo" class="logo"/>
         <h2>OLIVEIRA TRUST</h2>
       </div>
-      <div>
-        Profile
+      <div class="nav-bar__content">
+        <ProfileAvatar/>
+        <b class="nav-bar__content__username">Otávio Oliveira</b>
+        <IconButton name="arrow-right-from-bracket"/>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import IconButton from './IconButton'
+import ProfileAvatar from './ProfileAvatar'
+
 export default {
   name: 'NavBar',
+  components: {
+    IconButton,
+    ProfileAvatar,
+  }
 }
 </script>
 
 <style scoped>
-.nav-bar {
-  height: 80px;
-  width: 100vw;
-  background-color: #FFFFFF;
+h2 {
+  font-weight: 900;
 }
 
-.nav-bar__content {
-  padding: 20px;
+.nav-bar {
+  height: 80px;
+  background-color: #FFFFFF;
+  box-shadow: 0 2px #DDDDDD;
+  padding: 0 80px;
+}
+
+.nav-bar__contents {
+  padding: 20px 0;
   display: flex;
   justify-content: space-between;
   align-items: center;
 }
 
-.nav-bar__content__logomark {
+.nav-bar__content{
   display: flex;
   align-items: center;
 }
@@ -40,5 +54,9 @@ export default {
 .logo {
   margin-right: 20px;
   width: 40px;
+}
+
+.nav-bar__content__username {
+  margin: 0 10px;
 }
 </style>
