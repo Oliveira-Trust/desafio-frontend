4bd3d5d6f291225058f0f428851be04511a77cb0
MatheusT45 <matheus.tavares45@gmail.com>
2022-04-14 20:37:39 -0300

    feat: add profile avatar image and component

---
+++ src/components/ProfileAvatar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="profile-avatar">
+    <img :src="url" alt="User Profile Picture" class="profile-avatar__image">
+  </div>
+</template>
+
+<script>
+import defaultProfileAvatar from "../assets/profile-avatar.png"
+
+export default {
+  name: 'ProfileAvatar',
+  props: {
+    url: {
+      type: String,
+      default: defaultProfileAvatar
+    }
+  }
+}
+</script>
+
+<style scoped>
+.profile-avatar__image {
+  border: 1px solid #DDDDDD;
+  border-radius: 50%;
+  width: 40px;
+}
+
+</style>
