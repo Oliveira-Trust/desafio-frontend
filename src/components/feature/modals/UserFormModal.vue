3a6b06f97285e8b19189e692e86412b16c0cf268
Matheus Tavares <matheus.tavares45@gmail.com>
2022-05-07 00:22:02 -0300

    refactor: change the value input of the user form modal to be a buy in

--- src/components/feature/modals/UserFormModal.vue
+++ src/components/feature/modals/UserFormModal.vue
@@ -9,22 +9,30 @@
       <TextInput placeholder="Nome" v-model="user.nome"/>
       <TextInput placeholder="Sobrenome" v-model="user.sobrenome"/>
       <TextInput placeholder="E-mail" v-model="user.email"/>
-      <div class="wallet-value">
+      <div class="buy-in">
         <TextInput
           isCurrency
-          class="wallet-value-input"
+          class="buy-in-input"
           placeholder="Valor de compra"
-          v-model="userCurrency"
-          :readOnly="isEditing"
+          v-model="userCurrencyToAsk"
           :error="!isAmountValid"
         />
-        <h3 v-if="isAmountValid" class="wallet-value-btc-value" >BTC {{ userCryptoCurrency }}</h3>
-        <p v-else class="wallet-value-invalid">Valor inválido</p>
+        <h3 v-if="isAmountValid" class="buy-in-btc-value" >BTC {{ cryptoCurrencyAmoutToBuy }}</h3>
+        <p v-else class="buy-in-invalid">Valor inválido</p>
       </div>
-      <!-- <IconButton name="plus"/> -->
+      <div v-if="isEditing" class="wallet-value">
+        <b>Valor da Carteira</b>
+        <h3 v-if="isAmountValid" class="btc-full-value" >BTC {{ userTotalCryptoCurrency }}</h3>
+        <div v-else class="btc-full-value-loading"></div>
+      </div>
       <div class="user-modal-actions">
         <TextButton class="user-modal-actions-close" @onClick="close" label="Cancelar"/>
-        <Button class="user-modal-actions-submit" :disabled="!isEditing && !isFormValid" @onClick="save" label="Salvar"/>
+        <Button
+          class="user-modal-actions-submit"
+          :disabled="!isEditing && !isFormValid"
+          @onClick="save"
+          label="Salvar"
+        />
       </div>
     </template>
     </Modal>
@@ -37,7 +45,6 @@ import TextInput from '../../reusable/TextInput.vue'
 import Button from '../../reusable/Button.vue'
 import TextButton from '../../reusable/TextButton.vue'
 import {
-  getCurrencyValueByCryptoValue,
   getCryptoValueByCurrencyValue
 } from '../../../helpers/currency.utils'
 
@@ -65,36 +72,41 @@ export default {
   },
   data () {
     return {
-      userCurrency: 0,
-      userCryptoCurrency: ''
+      userCurrencyToAsk: '',
+      cryptoCurrencyAmoutToBuy: 0,
+      userTotalCryptoCurrency: 0,
     }
   },
   methods: {
     close () {
+      this.userCurrencyToAsk = '';
+      this.cryptoCurrencyAmoutToBuy = 0;
+      this.userTotalCryptoCurrency = 0;
       this.$emit('close');
     },
     async save () {
       if (this.isEditing) {
+        this.user.valor_carteira = parseFloat(this.userTotalCryptoCurrency);
         await this.$store.dispatch('updateUser', this.user);
         this.close();
         return;
       }
-      this.user.valor_carteira = this.userCryptoCurrency;
+      this.user.valor_carteira = this.cryptoCurrencyAmoutToBuy;
       await this.$store.dispatch('createUser', this.user);
       this.close();
     }
   },
   watch: {
     async isOpen () {
+      this.userCurrencyToAsk = '';
+      this.cryptoCurrencyAmoutToBuy = 0;
       await this.$store.dispatch('pullCryptoCurrencyPrice');
-      const currencyValue = getCurrencyValueByCryptoValue(this.user.valor_carteira, this.cryptoCurrencyValue);
-      this.userCurrency = this.user.valor_carteira ? currencyValue : 0;
-      this.userCryptoCurrency = this.isEditing ? this.user.valor_carteira : '0.00000000';
+      this.userTotalCryptoCurrency = this.user.valor_carteira;
     },
-    userCurrency (val) {
-      if (this.isEditing) return;
+    userCurrencyToAsk (val) {
       const cryptoValue = getCryptoValueByCurrencyValue(val, this.cryptoCurrencyValue);
-      this.userCryptoCurrency = cryptoValue;
+      this.cryptoCurrencyAmoutToBuy = cryptoValue;
+      this.userTotalCryptoCurrency = (this.user.valor_carteira + cryptoValue).toFixed(8);
     }
   },
   computed: {
@@ -105,7 +117,7 @@ export default {
       return !!this.user.id;
     },
     isAmountValid () {
-      return this.userCryptoCurrency || this.userCryptoCurrency === 0;
+      return this.cryptoCurrencyAmoutToBuy || this.cryptoCurrencyAmoutToBuy === 0;
     },
     isEmailValid () {
       return this.user.email
@@ -114,7 +126,7 @@ export default {
         && this.user.email.slice(-1) !== '.';
     },
     isFormValid () {
-      return parseFloat(this.userCurrency)
+      return parseFloat(this.userCurrencyToAsk)
         && this.user.nome
         && this.user.sobrenome
         && this.isEmailValid;
@@ -124,29 +136,42 @@ export default {
 </script>
 
 <style scoped>
-.wallet-value {
+.buy-in {
   display: flex;
   justify-content: space-between;
   align-items: center;
 }
 
-.wallet-value-input {
+.buy-in-input {
   width: 50%;
 }
 
-.wallet-value-btc-value {
+.buy-in-btc-value {
   margin-left: 10px;
   width: 50%;
 }
 
-.wallet-value-invalid {
+.wallet-value {
+  margin-top: 20px;
+  width: 100%;
+}
+
+.wallet-value > b {
+  color: #777;
+}
+
+.btc-full-value {
+  margin-top: 5px;
+}
+
+.buy-in-invalid {
   color: #E53935;
   margin-left: 10px;
   width: 50%;
 }
 
 .user-modal-actions {
-  margin: 40px 0 30px 0;
+  margin: 20px 0 30px 0;
   display: flex;
   justify-content: flex-end;
   align-items: center;
@@ -160,5 +185,21 @@ export default {
 .user-modal-actions-submit {
   margin-top: 10px;
   margin-left: 30px;
+}
+
+.btc-full-value-loading {
+  height: 30px;
+  width: 50%;
+  background: #eee;
+  background: linear-gradient(110deg, #ececec 8%, #f5f5f5 18%, #ececec 33%);
+  border-radius: 5px;
+  background-size: 200% 100%;
+  animation: 1.5s shine linear infinite;
+}
+
+@keyframes shine {
+  to {
+    background-position-x: -200%;
+  }
 }
 </style>
