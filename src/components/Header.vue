f269d6c62601e7e39d37a7d7ceff0ad8604d1405
Hallison <hallison.pm@gmail.com>
2022-08-26 12:03:13 -0300

    css header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="container c-header">
-    <img
-      src="@/assets/imgs/oliveira_trust.png"
-      alt="Oliveira Trust"
-      width="300"
-    />
+  <div class="c-header">
+    <div class="container">
+      <img
+        src="@/assets/imgs/oliveira_trust.png"
+        alt="Oliveira Trust"
+        width="300"
+      />
+    </div>
   </div>
 </template>
 
