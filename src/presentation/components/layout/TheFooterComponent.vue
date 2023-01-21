fb785c174aa7a1e837e12294ebd9fb848d5f391b
felipe leandro <foliveira.webdev@gmail.com>
2023-01-21 17:27:10 -0300

    feature:  add footer component

---
+++ src/presentation/components/layout/TheFooterComponent.vue
@@ -0,0 +1,13 @@
+<template>
+   <v-container>
+     <p class="text-center"> Fintools | Desafio Front-End</p>
+   </v-container>
+</template>
+
+<style lang="scss" scoped>
+@import '../../styles/variables.scss';
+
+p {
+  color: $txt-secondary-color
+}
+</style>
