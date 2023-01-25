259bf4fe2001518e0b991263de50742279da6e2f
felipe leandro <foliveira.webdev@gmail.com>
2023-01-25 11:10:42 -0300

    feature: add csv files support

---
+++ src/services/fileSystem/converter.js
@@ -0,0 +1,14 @@
+export const jsonToCSV = function(dataSource){
+    let csvContent = "data:text/csv;charset=utf-8,";
+    csvContent += [
+    Object.keys(dataSource[0]).join(";"),
+    ...dataSource.map(item => Object.values(item).join(";"))
+    ]
+    .join("\n")
+    .replace(/(^\[)|(\]$)/gm, "");
+    const data = encodeURI(csvContent);
+    return data
+}
+
+
+
---
+++ src/services/fileSystem/exporter.js
@@ -0,0 +1,6 @@
+export const fileExporter = function(file, fileName){  
+  const link = document.createElement("a");
+  link.setAttribute("href", file);
+  link.setAttribute("download", fileName);
+  link.click();    
+}
