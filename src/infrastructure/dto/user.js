21d13a4417aa96d20f2d181111e48d1df459b901
felipe leandro <foliveira.webdev@gmail.com>
2023-01-25 11:05:10 -0300

    refactor: update model and dto

--- src/infrastructure/dto/user.js
+++ src/infrastructure/dto/user.js
@@ -1,15 +1,15 @@
-class User {
+class UserDTO {
   constructor(args) {      
-    this.id = args?.id || null,
+    // this.id = args?.id || null,
     this.nome = args?.nome || null,
     this.sobrenome = args?.sobrenome || null,
     this.email = args?.email || null 
-    this.valor_carteira = args?.valor_carteira || null   
-    this.valor_currency = args?.valor_currency || args?.valor_carteira || null   
     this.endereco = args?.endereco || null,
     this.data_nascimento = args?.data_nascimento || null,
     this.data_abertura = args?.data_abertura || null,
-    this.endereco_carteira = args?.endereco_carteira || null
-  } 
+    this.endereco_carteira = args?.endereco_carteira || null,
+    this.valor_carteira = args?.valor_carteira || null   
+  }
 }
-module.exports = User;
+module.exports = UserDTO;
+
