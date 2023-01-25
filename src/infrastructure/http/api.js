23a4f9f643ad75887e11a11aa551f325a2373771
felipe leandro <foliveira.webdev@gmail.com>
2023-01-25 11:07:26 -0300

    feature: improve more verbs to api

--- src/infrastructure/http/api.js
+++ src/infrastructure/http/api.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-
 const axiosInstance = axios.create({
     baseURL: "http://localhost:3004",
    });
@@ -9,9 +8,25 @@ const get = async (url, params)=>{
 	const response = await axiosInstance.get(url, params)
 	return response.data;				
 }
+const post = async (url, params)=>{
+	const response = await axiosInstance.post(url, params)
+	return response.data;				
+}
+const put = async (url, params)=>{
+	const response = await axiosInstance.put(url + '/' + params.id, params)
+	return response.data;				
+}
+
+const remove = async (url, params)=>{
+	const response = await axiosInstance.delete(url + '/' + params)
+	return response.data;				
+}
 
 const api = {
-    get
+    get,
+    post,
+		put,
+    remove
 }
 
 export default api
