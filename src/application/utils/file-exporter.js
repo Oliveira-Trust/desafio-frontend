90a94d34ce468bd3ae2c9bc20642d30581647014
felipe leandro <foliveira.webdev@gmail.com>
2023-01-25 11:03:24 -0300

    chore: remove/move files

--- src/application/utils/file-exporter.js
+++
@@ -1,7 +0,0 @@
-const fileExporter = function(file, fileName){  
-  const link = document.createElement("a");
-  link.setAttribute("href", file);
-  link.setAttribute("download", fileName);
-  link.click();    
-}
-export default fileExporter
