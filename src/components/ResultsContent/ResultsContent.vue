66bb299c58aad0c129bdea6258fe39c25332b883
RudolfoSousa <rudolfosousa@gmail.com>
2023-06-14 15:22:07 -0300

    feat: create `ResultsContent` component

---
+++ src/components/ResultsContent/ResultsContent.vue
@@ -0,0 +1,62 @@
+<template>
+    <Paper>
+        <Heading text="Carteiras" link="Exportar CSV" />
+        <span class="divider"></span>
+        <List>
+            <ListItem
+            v-for="wallet in wallets"
+            :key="wallet.id"
+            :name="wallet.nome"
+            :surname="wallet.sobrenome"
+            :email="wallet.email"
+            :bitcoin="wallet.bitcoin"
+        />
+        </List>
+        <Footer
+            :count="pagination.count"
+            @onClick="updatePage"
+        ></Footer>
+    </Paper>
+</template>
+
+<script>
+    import Heading from '../shared/Heading/Heading.vue';
+    import Paper from '../shared/Paper/Paper.vue';
+    import List from './components/List.vue';
+    import ListItem from './components/ListItem.vue';
+    import Footer from './components/Footer.vue';
+    export default {
+        name: 'Content',
+        components: {
+            Heading,
+            Paper,
+            List,
+            ListItem,
+            Footer
+        },
+        methods: {
+            updatePage(page){
+                console.log(page);
+                this.$store.dispatch("fetchWallets", {page});
+            }
+        },
+        computed: {
+            wallets () {
+                return this.$store.state.wallets
+            },
+            pagination () {
+                console.log(this.$store.state.pagination)
+                return this.$store.state.pagination
+            }
+       },
+       mounted() {
+            this.$store.dispatch("fetchWallets", {page: 1});
+        }
+    }
+</script>
+
+<style>
+    .divider{
+        margin: 30px 0;
+    }
+</style>
