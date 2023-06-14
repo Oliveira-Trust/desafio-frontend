6d6cfea3d83d95ecc0a662dfe4515b6ffa3a7383
RudolfoSousa <rudolfosousa@gmail.com>
2023-06-14 15:18:54 -0300

    feat: create `Input` component

---
+++ src/components/shared/Input/Input.vue
@@ -0,0 +1,35 @@
+<template>
+    <input
+        class="input"
+        :placeholder="placeholder"
+        :value="modelvalue"
+        @input="$emit('update:modelvalue', $event.target.value)"
+    />
+</template>
+
+<script>
+    export default {
+        name: "BaseInput",
+        props:{
+            placeholder: String,
+            modelvalue: String
+        },
+        emits: ['update:modelvalue']
+    }
+</script>
+
+<style scoped>
+    .input{
+        border-radius: 3px;
+        border: 1px solid #e9e9e9;
+        padding: 10px;
+        width: 100%;
+    }
+    .input:focus{
+        outline: none;
+        border-color: var(--primary-color);
+    }
+    .input+.input{
+        margin-left: 10px;
+    }
+</style>
