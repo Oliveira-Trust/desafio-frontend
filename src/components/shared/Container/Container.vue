dfe2d9a03a7aee2bce844d10d4bd0b80f90b9a49
RudolfoSousa <rudolfosousa@gmail.com>
2023-06-14 15:18:23 -0300

    feat: create `Container` component

---
+++ src/components/shared/Container/Container.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Container"
+    }
+</script>
+
+<style scoped>
+    .container{
+        padding: 0 50px;
+    }
+</style>
