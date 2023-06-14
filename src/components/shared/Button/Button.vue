b1e3904560db7d2c0c498a2187dd77f9dac83294
RudolfoSousa <rudolfosousa@gmail.com>
2023-06-14 15:19:38 -0300

    feat: create `Button` component

---
+++ src/components/shared/Button/Button.vue
@@ -0,0 +1,16 @@
+<template>
+    <button class="button">
+        <slot></slot>
+    </button>
+</template>
+
+<style>
+.button{
+    border: 1px solid var(--primary-color);
+    color: var(--primary-color);
+    font-weight: 600;
+    background-color: #fff;
+    border-radius: 3px;
+    padding: 10px 20px;
+}
+</style>
