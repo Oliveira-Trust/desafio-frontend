2f5e1018940555eaf421a5af8f8e0ba8d72980aa
RudolfoSousa <rudolfosousa@gmail.com>
2023-06-14 15:18:40 -0300

    feat: create `Paper` component

---
+++ src/components/shared/Paper/Paper.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="paper">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default{
+        name: "Paper"
+    }
+</script>
+
+<style>
+.paper{
+    background-color: #fff;
+    padding: 15px 20px;
+    border-radius: 3px;
+    box-shadow: 2px 2px 5px 0px rgb(184, 184, 184);
+}
+</style>
