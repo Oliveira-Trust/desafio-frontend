4c554ba66c0f9081cc167b0ff139382728570968
RudolfoSousa <rudolfosousa@gmail.com>
2023-06-14 15:19:15 -0300

    feat: create `Heading` component

---
+++ src/components/shared/Heading/Heading.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+        <h1>{{ text }}</h1>
+        <Button>{{ link }}</Button>
+    </div>
+</template>
+
+<script>
+    import Button from '../Button/Button.vue';
+    export default {
+        name: "Heading",
+        components: {
+            Button
+        },
+        props: {
+            text: String,
+            link: String
+        }
+    }
+</script>
+
+<style scoped>
+.container{
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+}
+</style>
