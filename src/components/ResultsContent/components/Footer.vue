73c9991bbfacbd1e92d1f6dfe05833690b722e87
RudolfoSousa <rudolfosousa@gmail.com>
2023-06-14 15:21:31 -0300

    feat: create `ResultsContent`'s `Footer` component

---
+++ src/components/ResultsContent/components/Footer.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="footer">
+        <span>{{ count }} registro(s)</span>
+        <button @click="onClickButton">Next Page</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Footer",
+        props: {
+            count: Number
+        },
+        methods: {
+            onClickButton(){
+                this.$emit('onClick', 2)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+.footer{
+    margin-top: 30px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+.footer span{
+    color: #9b9b9b;
+    font-size: 12px;
+}
+</style>
