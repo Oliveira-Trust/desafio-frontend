1af6828acb58f7f4138c67b2ef886f1e8e6e11ad
Anderson Reis <andersonpensephp@hotmail.com>
2023-06-21 19:59:30 -0300

    feat: add bootstrap

---
+++ src/bootstrap.js
@@ -0,0 +1,7 @@
+import reboot from '../node_modules/bootstrap/dist/css/bootstrap-reboot.css'
+import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.css'
+
+export {
+  bootstrap,
+  reboot
+};
