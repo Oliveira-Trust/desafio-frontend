b15bb64736ff81774f85d4785cbcae5be8d581c3
Fábio Pinho <pinho.developer@gmail.com>
2024-04-21 20:14:45 -0300

    incremento da interface de wallet

--- src/components/Wallet/index.js
+++ src/components/Wallet/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+
+import { Title } from './style';
 
 const Wallet = () => {
   return (
     <Container>
       <Row>
         <Col>
-          BTC Carteiras
+          <Title className='mt-5 mb-4'>
+            BTC Carteiras
+          </Title>
         </Col>
-        <Col>
-          Adicionar Carteiras
+        <Col className='d-flex justify-content-end'>
+          <Button className='mt-5 mb-4 px-4 py-2 btn btn-primary'>
+            Adicionar Carteira
+          </Button>
         </Col>
       </Row>
     </Container>
---
+++ src/components/Wallet/style.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const Title = styled.h2`
+  font-size: 28px;
+  font-weight: bold;
+`;
