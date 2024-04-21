4ee24bfe4ba72f36899345264c2c9cb5631ceecf
Fábio Pinho <pinho.developer@gmail.com>
2024-04-21 20:38:19 -0300

    incremento da interface de header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,18 +1,32 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 
+import { User } from './style';
+
 const Header = () => {
   return (
-    <Container fluid>
-      <Row>
-        <Col>
-          Logo
-        </Col>
-        <Col>
-          Usuário
-        </Col>
-      </Row>
-    </Container>
+    <div className='py-3 bg-white'>
+      <Container>
+        <Row>
+          <Col>
+            <img src={require('../../assets/logo.png')} width={267} height={42} alt="Logo" />
+          </Col>
+          <Col className='d-flex justify-content-end'>
+            <Row>
+              <Col xs={3}>
+                <img src={require('../../assets/profile.png')} width={43} height={42} alt="Otávio Oliveira" />
+              </Col>
+              <Col className='d-flex align-items-center'>
+                <User>Otávio Oliveira</User>
+              </Col>
+              <Col className='d-flex align-items-center' xs={2}>
+                <img src={require('../../assets/logout.png')} width={26} height={26} alt="Logout" />
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 };
 
---
+++ src/components/Header/style.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const User = styled.span`
+  font-weight: bold;
+  font-size: 14px;
+  color: #3A3A3A;
+`;
