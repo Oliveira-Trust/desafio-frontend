dba8510f049e7e7a8983dc4782d7d61f46cafe24
Fábio Pinho <pinho.developer@gmail.com>
2024-04-21 20:34:24 -0300

    incremento da interface de filtro e comunicação com o redux

--- src/components/Filter/index.js
+++ src/components/Filter/index.js
@@ -1,24 +1,63 @@
-import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+
+import { Search } from 'react-bootstrap-icons';
+import { Container, Row, Col, FloatingLabel, Form, Button } from 'react-bootstrap';
+import { StyledFormControl } from './style';
+
+import { setEmail, setNome, setSobrenome } from '../../redux/filterSlice';
 
 const Filter = () => {
+  const dispatch = useDispatch();
+  
+  // state para controle da informação no form
+  // para previnir o consumo desnecessário da API 
+  const [inputNome, setInputNome] = useState('');
+  const [inputSobrenome, setInputSobrenome] = useState('');
+  const [inputEmail, setInputEmail] = useState('');
+
+  // update do redux para promover a informação para
+  // os outros componentes
+  const handleSearch = () => {
+    dispatch(setNome(inputNome.trim()));
+    dispatch(setSobrenome(inputSobrenome.trim()));
+    dispatch(setEmail(inputEmail.trim()));
+  };
+
   return (
-    <Container>
-      <Row>
-        <Col>
-          Nome
-        </Col>
-        <Col>
-          Sobrenome
-        </Col>
-        <Col>
-          E-mail
-        </Col>
-        <Col>
-          Busca
-        </Col>
-      </Row>
-    </Container>
+    <Form onSubmit={(e) => { e.preventDefault(); handleSearch(); }}>
+      <Container>
+        <Row>
+          <Col>
+            <div className='p-3 mb-4 rounded shadow bg-white'>
+              <Row>
+                <Col>
+                  <FloatingLabel controlId="floatingInput" label="Nome">
+                    <StyledFormControl type="text" placeholder='Nome' value={inputNome} onChange={(e) => setInputNome(e.target.value)} />
+                  </FloatingLabel>
+                </Col>
+                <Col>
+                  <FloatingLabel controlId="floatingPassword" label="Sobrenome">
+                    <StyledFormControl type="text" placeholder="Sobrenome" value={inputSobrenome} onChange={(e) => setInputSobrenome(e.target.value)} />
+                  </FloatingLabel>
+                </Col>
+                <Col>
+                  <FloatingLabel controlId="floatingPassword" label="E-mail">
+                    <StyledFormControl type="email" placeholder="E-mail" value={inputEmail} onChange={(e) => setInputEmail(e.target.value)} />
+                  </FloatingLabel>
+                </Col>
+                <Col xs={2} className='d-grid gap-2'>
+                  <Button type="submit" variant="outline-primary" onClick={handleSearch}>
+                    <Search />
+                    &nbsp;&nbsp;<strong>Buscar</strong>
+                  </Button>
+                </Col>
+              </Row>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </Form>
   );
 };
 
---
+++ src/components/Filter/style.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+import { Form } from 'react-bootstrap';
+
+export const StyledFormControl = styled(Form.Control)`
+  min-height: 52px !important;
+  max-height: 52px !important;
+`;
