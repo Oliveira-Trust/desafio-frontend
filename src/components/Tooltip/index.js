df5b604e4d2945f63671d74ea02275c1d09824fe
Fábio Pinho <pinho.developer@gmail.com>
2024-04-21 22:12:56 -0300

    Centralizando tooltips

---
+++ src/components/Tooltip/index.js
@@ -0,0 +1,13 @@
+import { Tooltip } from "react-bootstrap";
+
+export const tooltipEdit = (
+  <Tooltip id="tooltip">
+    Editar
+  </Tooltip>
+);
+
+export const tooltipDelete = (
+  <Tooltip id="tooltip">
+    Excluir
+  </Tooltip>
+);
