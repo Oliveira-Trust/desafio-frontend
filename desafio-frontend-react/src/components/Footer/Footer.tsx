d01e00cce2a2b7299ad42c2fe2f7395cac5aec40
unknown <raineenf@gmail.com>
2024-04-30 01:35:33 -0300

    criado footer

---
+++ desafio-frontend-react/src/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import styles from "./Footer.module.css";
+
+export function Footer() {
+  return (
+    <div className={styles.footerContainer}>
+      <span className={styles.footerText}>Fintools | Desafio Front-End</span>
+    </div>
+  );
+}
