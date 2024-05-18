8aecc29fcddf709f94b1a183260c09ab0b58944b
alvesxdani <dani.alvesm22@gmail.com>
2024-05-18 17:26:43 -0300

    :sparkles: adicionando o cabeçalho da tabela

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,29 @@
 <template>
+  <div id="app">
+    <!-- Header da página -->
     <Header />
+
+    <div class="container">
+      <!-- Título e botão de adicionar -->
+      <CabecalhoTabela />
+    </div>
+  </div>
 </template>
 
 <script>
+import 'primeicons/primeicons.css'
 import Header from './components/Header/index.vue'
+import CabecalhoTabela from './Containers/CabecalhoTabela/index.vue'
 
 export default {
   name: 'App',
   components: {
-    Header
-  }
+    Header,
+    CabecalhoTabela,
+  },
 }
 </script>
 
 <style>
-#app {
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #333;
-}
+@import './styles/global.css';
 </style>
