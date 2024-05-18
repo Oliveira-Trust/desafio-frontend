838faa997fe5ce3683d6b1195381a6d29bf17683
alvesxdani <dani.alvesm22@gmail.com>
2024-05-18 20:43:42 -0300

    :sparkles: criando componente de Box

---
+++ src/components/BoxContent/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container-content">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BoxContent',
+}
+</script>
+
+<style scoped>
+@import './styles.css';
+</style>
