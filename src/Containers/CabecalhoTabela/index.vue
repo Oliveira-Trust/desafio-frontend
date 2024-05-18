cbdb7c2645588a9d60f9cc65a7d8a114a0e87c2f
alvesxdani <dani.alvesm22@gmail.com>
2024-05-18 17:25:22 -0300

    :tada: init: initial commit

---
+++ src/Containers/CabecalhoTabela/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Titulo :titulo="'BTC Carteiras'" />
+    <Button :text="Botão" />
+  </div>
+</template>
+
+<script>
+import Button from '../../components/Button/index.vue'
+import Titulo from '../../components/Titulo/index.vue'
+
+export default {
+  name: 'CabecalhoTabela',
+  components: {
+    Button,
+    Titulo,
+  },
+}
+</script>
+
+<style></style>
