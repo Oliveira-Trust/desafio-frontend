200dfecddf6bff766d5f450431f405d761ac95ee
alvesxdani <dani.alvesm22@gmail.com>
2024-05-18 16:59:04 -0300

    :broom: cleanup: linhas desnecessárias

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,10 @@
-<!-- Componente Header -->
 <template>
-  <div id="app">
     <Header />
-  </div>
 </template>
 
 <script>
 import Header from './components/Header/index.vue'
+
 export default {
   name: 'App',
   components: {
@@ -17,7 +15,6 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #333;
