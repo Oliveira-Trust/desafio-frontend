d0acb8aa7d9a0bcbce2d0ad6002670c0100bf8de
alvesxdani <dani.alvesm22@gmail.com>
2024-05-18 14:08:08 -0300

    :spakles: adicionando o componente header

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
+<!-- Componente Header -->
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Header from './components/Header/index.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
@@ -21,8 +20,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #333;
 }
 </style>
