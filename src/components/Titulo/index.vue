463b289ab948f0d1862813a959508c5ad76af27d
alvesxdani <dani.alvesm22@gmail.com>
2024-05-18 18:40:04 -0300

    :sparkles: adicionando lógica dos tipos de título

--- src/components/Titulo/index.vue
+++ src/components/Titulo/index.vue
@@ -1,17 +1,29 @@
 <template>
-  <h1>{{ titulo }}</h1>
+  <component :is="tag">{{ text }}</component>
 </template>
 
 <script>
 export default {
   name: 'Titulo',
   props: {
-    titulo: {
+    text: {
       type: String,
       required: true,
+    },
+    level: {
+      type: String,
+      default: 'h1',
+      validator: (value) => ['h1', 'h2', 'h3'].includes(value),
+    },
+  },
+  computed: {
+    tag() {
+      return this.level
     },
   },
 }
 </script>
 
-<style></style>
+<style scoped>
+@import './style.css'
+</style>
