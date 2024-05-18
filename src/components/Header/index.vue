1dd5b24f2448d5f83a2115a97c0d1dd1e9ce545b
alvesxdani <dani.alvesm22@gmail.com>
2024-05-18 16:58:23 -0300

    :sparkles: finalização do componente Header

---
+++ src/components/Header/index.vue
@@ -0,0 +1,31 @@
+<!-- Componente Header -->
+<template>
+  <header>
+    <img :src="svgUrl" alt="Logo" class="logo" />
+
+    <div class="user">
+      <i class="pi pi-user user-svg"></i>
+      <span class="user-name">Otávio Oliveira</span>
+      <i class="pi pi-sign-out exit-svg"></i>
+    </div>
+  </header>
+</template>
+
+<script>
+import svg from '@/assets/brand.svg'
+
+export default {
+  name: 'Header',
+  components: {
+  },
+  data() {
+    return {
+      svgUrl: svg,
+    }
+  },
+}
+</script>
+
+<style scoped>
+@import './styles.css';
+</style>
