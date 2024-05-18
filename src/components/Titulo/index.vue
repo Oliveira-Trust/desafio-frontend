ba2af182303dfd6955d78f4221823581fdec207e
alvesxdani <dani.alvesm22@gmail.com>
2024-05-18 17:26:10 -0300

    :tada: init: initial commit

---
+++ src/components/Titulo/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>{{ titulo }}</h1>
+</template>
+
+<script>
+export default {
+  name: 'Titulo',
+  props: {
+    titulo: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style></style>
