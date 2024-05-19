afb5eec907c07434ebdc69df4c40c3cae61ec187
alvesxdani <dani.alvesm22@gmail.com>
2024-05-19 16:00:14 -0300

    :truck: chore: renomeando o componente para Title

---
+++ src/components/Title/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <component :is="tag">{{ text }}</component>
+</template>
+
+<script>
+export default {
+  name: 'Title',
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+    level: {
+      type: String,
+      default: 'h1',
+      validator: (value) => ['h1', 'h2', 'h3'].includes(value),
+    },
+  },
+  computed: {
+    tag() {
+      return this.level
+    },
+  },
+}
+</script>
+
+<style scoped>
+@import './style.css'
+</style>
