e19148627359001ff751f652d015e9bde81d7fa8
alvesxdani <dani.alvesm22@gmail.com>
2024-05-19 16:21:20 -0300

    :sparkles: finalizando a estrutura da tabela

--- src/Containers/Tabela/index.vue
+++ src/Containers/Tabela/index.vue
@@ -1,15 +1,59 @@
 <template>
-  <div>
-    Olá mundo
-  </div>
+  <BoxContent>
+    <div class="header">
+      <Title :text="'Carteiras'" :level="'h2'">Test</Title>
+      <Button :color="'outlined'">Exportar CSV</Button>
+    </div>
+
+    <table tabindex="fixed">
+      <thead>
+        <tr>
+          <th>Nome</th>
+          <th>Sobrenome</th>
+          <th>E-mail</th>
+          <th>Bitcoin</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>a</td>
+          <td>a</td>
+          <td>a</td>
+          <td>a</td>
+        </tr>
+        <tr>
+          <td>a</td>
+          <td>a</td>
+          <td>a</td>
+          <td>a</td>
+        </tr>
+      </tbody>
+      <tfoot>
+        <span class="registros">30 registros</span>
+        <Pagination />
+      </tfoot>
+    </table>
+  </BoxContent>
 </template>
 
 <script>
+import BoxContent from '../../components/BoxContent/index.vue'
+import Title from '../../components/Title/index.vue'
+import Button from '../../components/Button/index.vue'
+import Pagination from '../../components/Pagination/index.vue'
+
 export default {
   name: 'Tabela',
+  components: {
+    BoxContent,
+    Title,
+    Button,
+    Pagination,
+  },
 }
 </script>
 
 <style scoped>
-@import './styles.css'
+@import './styles.css';
 </style>
