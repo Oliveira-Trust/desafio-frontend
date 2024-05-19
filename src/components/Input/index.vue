38c8db756e63210dafebcc669d35f6fafa0009ad
alvesxdani <dani.alvesm22@gmail.com>
2024-05-19 01:49:08 -0300

    :sparkles: finalização da criação do componente Botão

---
+++ src/components/Input/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <input :type="type" :placeholder="placeholder" :value="value" @input="onChange" />
+</template>
+
+<script>
+export default {
+  name: 'Input',
+  props: {
+    type: {
+      type: String,
+      default: 'text',
+      validate: (value) => ['text', 'email'].includes(value),
+      required: true,
+    },
+    placeholder: {
+      type: String,
+      default: '',
+    },
+    value: {
+      type: String,
+      default: '',
+      required: true
+    },
+  },
+  methods: {
+    onChange(event) {
+      this.$emit('input', event.target.value);
+    }
+  }
+}
+</script>
+
+<style>
+@import './styles.css';
+</style>
