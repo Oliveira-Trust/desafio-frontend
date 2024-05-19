d41337e0837a2a58e8a01ac78c48e0630df4956b
alvesxdani <dani.alvesm22@gmail.com>
2024-05-19 16:01:56 -0300

    :recycle: refactor: Renomeando componente importado Title

--- src/Containers/CabecalhoTabela/index.vue
+++ src/Containers/CabecalhoTabela/index.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="cabecalho">
-    <Titulo :text="'BTC Carteiras'" :level="'h1'"/>
+    <Title :text="'BTC Carteiras'" :level="'h1'"/>
     <Button :color="'primary'">Adicionar carteira</Button>
   </div>
 </template>
 
 <script>
 import Button from '../../components/Button/index.vue'
-import Titulo from '../../components/Titulo/index.vue'
+import Title from '../../components/Title/index.vue'
 
 export default {
   name: 'CabecalhoTabela',
   components: {
     Button,
-    Titulo,
+    Title,
   },
 }
 </script>
