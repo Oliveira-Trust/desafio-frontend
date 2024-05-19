cceca198ce00adb5075f90e7d958e61bfb4260f3
alvesxdani <dani.alvesm22@gmail.com>
2024-05-19 01:49:44 -0300

    :recycle: refactor: renomeando a propriedade de cor do botão

--- src/Containers/CabecalhoTabela/index.vue
+++ src/Containers/CabecalhoTabela/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="cabecalho">
     <Titulo :text="'BTC Carteiras'" :level="'h1'"/>
-    <Button :text="'Adicionar carteira'" :type="'primary'" />
+    <Button :text="'Adicionar carteira'" :color="'primary'" />
   </div>
 </template>
 
