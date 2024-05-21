07e990282d99282181aec121b0e4ddfdf17cce9d
Matheus Tavares <matheus.tavares45@gmail.com>
2024-05-21 20:59:25 -0300

    feat(profileAvatar): adds component for profile avatar

---
+++ src/components/feature/ProfileAvatar.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import defaultProfileAvatar from '@/assets/profile-avatar.png'
+
+defineProps({
+  url: {
+    type: String,
+    default: defaultProfileAvatar
+  }
+})
+</script>
+<template>
+  <div>
+    <img :src="url" alt="User Profile Picture" class="w-7 rounded-full" />
+  </div>
+</template>
