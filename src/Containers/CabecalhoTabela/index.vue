0fb283cf3b67a9c8fccce341f165f0a2178949eb
alvesxdani <dani.alvesm22@gmail.com>
2024-05-20 22:27:40 -0300

    :sparkles: adicionando o modal de criação de carteira

--- src/Containers/CabecalhoTabela/index.vue
+++ src/Containers/CabecalhoTabela/index.vue
@@ -1,19 +1,22 @@
 <template>
   <div class="cabecalho">
-    <Title :text="'BTC Carteiras'" :level="'h1'"/>
+    <Title :text="'BTC Carteiras'" :level="'h1'" />
     <Button :color="'primary'">Adicionar carteira</Button>
+    <AddCarteiraModal :user="selectedUser" :visible="isEditModalVisible" @close="isEditModalVisible = false" @save="editUser" />
   </div>
 </template>
 
 <script>
 import Button from '../../components/Button/index.vue'
 import Title from '../../components/Title/index.vue'
+import AddCarteiraModal from '../AddCarteiraModal/index.vue'
 
 export default {
   name: 'CabecalhoTabela',
   components: {
     Button,
     Title,
+    AddCarteiraModal
   },
 }
 </script>
