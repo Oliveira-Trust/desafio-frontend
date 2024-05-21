8c96f9c8b18532db5278cc96f69b87d09e767385
Matheus Tavares <matheus.tavares45@gmail.com>
2024-05-21 18:54:30 -0300

    refactor(vue): upgrade vue2 to vue3 and add tailwind

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import App from './App.vue'
+import './index.css'
+
+const app = createApp(App)
+
+app.use(createPinia())
+
+app.mount('#app')
