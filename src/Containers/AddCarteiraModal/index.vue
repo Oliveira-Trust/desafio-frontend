30655e69cb0d879ca1c4a94a9abb73423a4ac3c0
alvesxdani <dani.alvesm22@gmail.com>
2024-05-21 00:04:02 -0300

    :sparkles: finalização do componente de adicionar nova carteira

--- src/Containers/AddCarteiraModal/index.vue
+++ src/Containers/AddCarteiraModal/index.vue
@@ -1,13 +1,108 @@
 <template>
-  <div>ola mundo</div>
+  <div class="modal" v-if="visible">
+    <div class="modal-content">
+      <button class="close" @click="closeModal">
+        <i class="pi pi-times"></i>
+      </button>
+      <Title :level="'h2'" :text="'Adicionar carteira'" />
+      <form @submit.prevent="submitAdd" class="form-edit">
+        <div class="row">
+          <Input v-model="newUser.nome" :type="'text'" :placeholder="'Nome'" id="nome" required />
+        </div>
+        <div class="row">
+          <Input v-model="newUser.sobrenome" :type="'text'" :placeholder="'Sobrenome'" id="sobrenome" required />
+        </div>
+        <div class="row">
+          <Input v-model="newUser.email" :type="'email'" :placeholder="'E-mail'" id="email" required />
+        </div>
+        <div class="row">
+          <select v-model="selectedCurrency" @change="fetchExchangeRate">
+            <option v-for="currency in currencies" :key="currency" :value="currency">{{ currency }}</option>
+          </select>
+          <Input v-model.number="currencyValue" :type="'number'" step="0.01" :placeholder="'Valor'" id="currencyValue" required />
+          <span>BTC {{ btcValue }}</span>
+        </div>
+        <Button :color="'primary'" type="submit">Adicionar</Button>
+        <Button :color="'outlined'" type="button" @click="closeModal">Cancelar</Button>
+      </form>
+    </div>
+  </div>
 </template>
 
 <script>
+import axios from 'axios'
+import Title from '../../components/Title/index.vue'
+import Input from '../../components/Input/index.vue'
+import Button from '../../components/Button/index.vue'
+
 export default {
+  name: 'AddCarteiraModal',
+  components: {
+    Title,
+    Input,
+    Button
+  },
+  props: {
+    visible: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      newUser: {
+        nome: '',
+        sobrenome: '',
+        email: '',
+        valor_carteira: 0
+      },
+      selectedCurrency: 'USD',
+      exchangeRate: 0,
+      currencies: ['USD', 'EUR', 'BRL'],
+      currencyValue: 0,
+      btcValue: 0
+    }
+  },
+  methods: {
+    closeModal() {
+      this.$emit('close')
+    },
+    async submitAdd() {
+      await this.convertToBTC()
+      this.$emit('add', { ...this.newUser })
+      this.newUser = {
+        nome: '',
+        sobrenome: '',
+        email: '',
+        valor_carteira: 0
+      }
+      this.closeModal()
+    },
+    async fetchExchangeRate() {
+      try {
+        const response = await axios.get(`https://economia.awesomeapi.com.br/last/BTC-${this.selectedCurrency}`)
+        const rate = response.data[`BTC${this.selectedCurrency}`].bid
+        this.exchangeRate = parseFloat(rate)
+        await this.convertToBTC()
+      } catch (error) {
+        console.error('Erro ao buscar taxa de câmbio:', error)
+      }
+    },
+    async convertToBTC() {
+      if (this.exchangeRate && this.currencyValue) {
+        this.btcValue = parseFloat((this.currencyValue / this.exchangeRate).toFixed(8))
+      }
+    }
+  },
+  watch: {
+    currencyValue(newValue) {
+      this.fetchExchangeRate(newValue)
+    }
+  }
 
 }
 </script>
 
-<style>
-
+<style scoped>
+@import '../EditCarteiraModal/styles.css'
 </style>
