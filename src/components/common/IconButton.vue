24476db98661171c51a22164fdd659e347aa93e4
Matheus Tavares <matheus.tavares45@gmail.com>
2024-05-21 20:24:24 -0300

    feat(build): adds fontawesome lib and icon button component

---
+++ src/components/common/IconButton.vue
@@ -0,0 +1,83 @@
+<script setup lang="ts">
+import { defineProps, defineEmits } from 'vue'
+
+const emit = defineEmits(['onClick'])
+
+defineProps({
+  name: {
+    type: String,
+    default: 'pencil-alt'
+  },
+  tooltipText: {
+    type: String,
+    default: undefined
+  }
+})
+
+const onClick = () => {
+  emit('onClick')
+}
+</script>
+
+<template>
+  <div class="icon-button" @click="onClick">
+    <font-awesome-icon :icon="['fas', name]" />
+    <span v-if="tooltipText" class="tooltip-text">{{ tooltipText }}</span>
+  </div>
+</template>
+
+<style scoped>
+.icon-button {
+  position: relative;
+  height: 24px;
+  width: 24px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 3px 2px;
+  border-radius: 50%;
+  cursor: pointer;
+}
+
+.icon-button:hover {
+  background-color: #dfdfdf;
+}
+
+.icon {
+  fill: #656565;
+}
+
+.icon-button .tooltip-text {
+  font-size: 14px;
+  visibility: hidden;
+  min-width: 60px;
+  background-color: #424242;
+  color: #fff;
+  text-align: center;
+  padding: 7px 0;
+  border-radius: 6px;
+  position: absolute;
+  z-index: 1;
+  top: 120%;
+  left: 50%;
+  margin-left: -30px;
+  opacity: 0;
+  transition: opacity 0.2s;
+}
+
+.icon-button .tooltip-text::after {
+  content: ' ';
+  position: absolute;
+  bottom: 100%;
+  left: 50%;
+  margin-left: -5px;
+  border-width: 5px;
+  border-style: solid;
+  border-color: transparent transparent #424242 transparent;
+}
+
+.icon-button:hover .tooltip-text {
+  visibility: visible;
+  opacity: 1;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import './index.css'
 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { faTwitter, faFontAwesome } from '@fortawesome/free-brands-svg-icons'
+
+library.add(fas, faTwitter, faFontAwesome)
+
 const app = createApp(App)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.use(createPinia())
 
