efda8bc8ff919866c97d1881e85cb2b6bd4cdb5e
Matheus Tavares <matheus.tavares45@gmail.com>
2024-05-22 18:54:13 -0300

    feat(card): adds common card component

---
+++ src/components/common/CommonCard.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="bg-white rounded [box-shadow:#0000003D_0px_3px_8px] p-5">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped></style>
