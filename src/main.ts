c902c0d578115fbb915137ecfa001ea6299e61a7
Matheus Tavares <matheus.tavares45@gmail.com>
2024-05-22 19:39:13 -0300

    chore(ts): adds navigator msSaveBlob type

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,12 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { faTwitter, faFontAwesome } from '@fortawesome/free-brands-svg-icons'
 
+declare global {
+  interface Navigator {
+      msSaveBlob?: (blob: Object, defaultName?: string) => boolean
+  }
+}
+
 library.add(fas, faTwitter, faFontAwesome)
 
 const app = createApp(App)
