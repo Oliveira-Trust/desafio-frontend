9ebd16a5abc1c844d8cf4051b29c5bfdc7d6c10b
Matheus Tavares <matheus.tavares45@gmail.com>
2024-05-22 19:10:29 -0300

    feat(types): adds user, filter and pagination types

---
+++ src/types/filter.type.ts
@@ -0,0 +1,5 @@
+export type FilterData = {
+  nome: string;
+  sobrenome: string;
+  email: string;
+}
---
+++ src/types/pagination.type.ts
@@ -0,0 +1,5 @@
+export type PaginationData = {
+  totalItems: number;
+  currentPage: number;
+  pages: number;
+}
