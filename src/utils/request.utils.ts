132b2aab9d9e581213f311ef37e44df9fd8eb44d
Matheus Tavares <matheus.tavares45@gmail.com>
2024-05-23 02:08:41 -0300

    removes unecessary cors headers

--- src/utils/request.utils.ts
+++ src/utils/request.utils.ts
@@ -1,18 +1,8 @@
 export const makeRequest = async (
   url = '',
-  handleCors = false,
-  options = {}
+  options = {},
+  customHeaders = {}
 ) => {
-  let customHeaders = {};
-  if (handleCors)
-    customHeaders = {
-      'Access-Control-Allow-Methods': 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': 'true',
-      'Access-Control-Allow-Headers':
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-    };
-
   return await fetch(url, {
     ...options,
     headers: {
