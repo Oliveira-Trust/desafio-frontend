cd9de12a2de30deab3a26fc3b6e4d878c14f65b3
Matheus Tavares <matheus.tavares45@gmail.com>
2024-05-23 00:23:57 -0300

    feat(textButton): adds text button component

---
+++ src/components/common/TextButton.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+interface Props {
+  label: string
+}
+
+const emit = defineEmits(['onClick'])
+withDefaults(defineProps<Props>(), {
+  label: 'Cancelar'
+})
+
+const onClick = () => {
+  emit('onClick')
+}
+</script>
+<template>
+  <b class="cursor-pointer text-blue-400 hover:text-blue-500" @click="onClick">{{ label }}</b>
+</template>
+<style scoped></style>
