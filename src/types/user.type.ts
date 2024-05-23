e15fa3170f1a1cb89c9547abda47dc6b089c9aec
Matheus Tavares <matheus.tavares45@gmail.com>
2024-05-23 02:05:46 -0300

    refactor(type): changes some user attributes to be optional

--- src/types/user.type.ts
+++ src/types/user.type.ts
@@ -3,9 +3,9 @@ export type User = {
   nome: string;
   sobrenome: string;
   email: string;
-  endereco: string;
-  data_nascimento: string;
-  data_abertura: string;
-  valor_carteira: number;
-  endereco_carteira: string;
+  endereco?: string;
+  data_nascimento?: string;
+  data_abertura?: string;
+  valor_carteira?: number;
+  endereco_carteira?: string;
 };
