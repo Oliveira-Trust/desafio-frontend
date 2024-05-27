ac78889bf535bcaa2a1ca3c6a12d65386306dbe9
alvesxdani <dani.alvesm22@gmail.com>
2024-05-26 21:23:16 -0300

    :recycle: refactor: refatoração para adicionar a funcionalidade

--- src/Containers/FormularioFiltro/index.vue
+++ src/Containers/FormularioFiltro/index.vue
@@ -1,15 +1,16 @@
 <template>
   <BoxContent>
     <form @submit.prevent="submitForm" class="form">
-      <Input type="text" placeholder="Nome" v-model="name" />
+      <Input type="text" placeholder="Nome" v-model="nome" />
       <Input type="text" placeholder="Sobrenome" v-model="sobrenome" />
       <Input type="email" placeholder="E-mail" v-model="email" />
-      <Button :color="'outlined'" type="submit" ><i class="pi pi-search"></i> Buscar</Button>
+      <Button :color="'outlined'" type="submit"><i class="pi pi-search"></i> Buscar</Button>
     </form>
   </BoxContent>
 </template>
 
 <script>
+import { mapGetters, mapActions } from 'vuex'
 import BoxContent from '../../components/BoxContent/index.vue'
 import Input from '../../components/Input/index.vue'
 import Button from '../../components/Button/index.vue'
@@ -21,20 +22,37 @@ export default {
     Input,
     Button,
   },
-  data() {
-    return {
-      name: '',
-      sobrenome: '',
-      email: '',
-    }
-  },
-  methods: {
-    submitForm() {
-      console.log(this.name)
-      console.log(this.sobrenome)
-      console.log(this.email)
+  props: {
+    name: {
+      type: String,
+      default: '',
+    },
+    lastName: {
+      type: String,
+      default: '',
+    },
+    emailUser: {
+      type: String,
+      default: '',
     },
   },
+  computed: {
+    ...mapGetters('users', ['users']),
+  },
+  methods: {
+    ...mapActions('users', ['filterUsers', 'fetchUsers']),
+    submitForm() {
+      this.filterUsers({ nome: this.nome, sobrenome: this.sobrenome, email: this.email })
+      if(this.nome === '' && this.sobrenome === '' && this.email === '') this.fetchUsers()
+    },
+  },
+  data() {
+    return {
+      nome: this.name,
+      sobrenome: this.lastName,
+      email: this.emailUser,
+    }
+  }
 }
 </script>
 
