7728999a2aa2ef261ade8244ac923f0618afceaa
Breno Pereira <brenopereira856@gmail.com>
2025-08-12 23:33:02 -0300

    chore: add favicon and title for page

--- panel/nuxt.config.ts
+++ panel/nuxt.config.ts
@@ -18,6 +18,10 @@ export default defineNuxtConfig({
   },
   app: {
     head: {
+      title: 'OT | Wallets',
+      link: [
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }
+      ],
       viewport: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no',
       meta: [
         { name: 'format-detection', content: 'telephone=no' },
